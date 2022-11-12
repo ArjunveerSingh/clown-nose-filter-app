1e29b72e66badbbb32135ee3461a7071c7f23905
ArjunveerSingh <76784986+ArjunveerSingh@users.noreply.github.com>
2022-11-12 17:37:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video= createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet= ml5.poseNet(video,modelLoaded);
+    poseNet.on('Pose', gotResults);
+}
+
+function modelLoaded(){
+console.log("Model Loaded");
+}
+
+function gotResults(results){
+    if(results.length > 0){
+        console.log(results);
+    }
+}
+
+function draw(){
+    image(video,0,0,300,300);
+}
+
+function click(){
+    save('FilteredImage.png');
+}
